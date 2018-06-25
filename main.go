5374ea0dfeafac2b797882ff49cc81d145e11dfb
James Wynne <jwynne@pivotal.io>
2018-06-25 22:06:12 +0100

    Group looks cleaner and has an _all_ option

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	game := flag.String("g","","Please provide a valid teams use -t to get valid countries")
 	team := flag.Bool("t",false,"This will provide you a list of countries")
+	groupList := flag.Bool("gl",false,"This will provide you a list of groups")
 	group := flag.String("gp","","List of groups")
 
 	flag.Parse()
@@ -30,6 +31,17 @@ func main() {
 		games.Getgames(bodyBytes, *game)
 	}
 
+	if *groupList == true {
+		url := "https://worldcup.sfg.io/teams/group_results"
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("%s", err)
+		}
+
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(groups.GetGroupList(bodyBytes))
+	}
+
 	if *team == true {
 		url := "http://worldcup.sfg.io/teams"
 		resp, err := http.Get(url)
@@ -43,15 +55,22 @@ func main() {
 	}
 
 	if len(*group) != 0 {
+
 		url := "https://worldcup.sfg.io/teams/group_results"
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Printf("%s", err)
 		}
-
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 
-		groups.GetGroup(bodyBytes, *group)
+		if *group == "all" {
+			list := groups.GetGroupList(bodyBytes)
+			for x := range list {
+				groups.GetGroup(bodyBytes, list[x])
+			}
+		} else {
+			groups.GetGroup(bodyBytes, *group)
+		}
 
 	}
 
